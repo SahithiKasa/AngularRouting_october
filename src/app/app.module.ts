b16f1b11dfe889e10573c3ab5d50fb20531388b3
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-10-17 10:40:38 +0530

    Routing1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule , Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+//path = address
+
+// 1. based on your path(ex:home)      ---> homelocation(homecompoennt)
+// 2. based on your path(ex:aboutus)   ---> aboutus(aboutuscompoennt)
+
+
+//step 1 : Declare the routes
+const routes: Routes = [
+  // {
+  //   path: '',                   //https://locahost:4200
+  //   component:HomeComponent,
+  // },
+  {
+    path: '',  
+     redirectTo:'aboutus',
+     pathMatch:'full'                //https://locahost:4200
+   
+  },
+  {
+    path: 'home',                //https://locahost:4200/home
+    component: HomeComponent,
+  },
+  {
+    path: 'aboutus',             //https://locahost:4200/aboutus
+    component: AboutusComponent,
+  },
+  {
+    path:'**',                  //https://localhost:4200/akkkkkagbgggg
+    component:NotfoundComponent
+  }
+  // {
+  //   path:'**',                  //https://localhost:4200/akkkkkagbgggg
+  //   redirectTo:'home',
+  //   pathMatch:'full'
+  // }
+];
+
+//https://loclhost:4200/home    ----> home component
+//https://loclhost:4200/aboutus ----> aboutus componet
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutusComponent, NotfoundComponent],
+  imports: [BrowserModule , RouterModule.forRoot(routes)],         //register the routes
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
